95d4774cdd164a87459b526a3f84f32e7d28feca
VikasAdke2023 <128823974+VikasAdke2023@users.noreply.github.com>
2023-03-24 20:18:27 +0530

    First Commit

---
+++ main.tf
@@ -0,0 +1,36 @@
+provider "aws"{
+access_key="AKIAQTKQW4RXXPC2LA6M"
+secret_key="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"
+region="us-east-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  count=1
+  tags = {
+    Name = "Machine-${count.index}"
+  }
+}
+
+variable "count"{
+}
+
+variable "name"{
+}
